983a46ebcfbe2b9b3cb3c6aa1e2ac4c9d13061ba
Ravindra Bhagwandin <rbhagwandin@gmail.com>
2021-02-04 18:32:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XBSDataScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
