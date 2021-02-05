e4028e83cc6d4627b51b6b6f791362929b2bd4e3
Ravindra Bhagwandin <rbhagwandin@gmail.com>
2021-02-04 22:36:55 -0500

    Change DLL reference to nuget package. Refactored code to support

--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using XBS.Core;
 
 namespace XBSDataScraper
 {
     class Program
     {
-        static void Main(string[] args)
+
+        // The target season for which data will primarily be pulled
+        static string TARGET_SEASON = "30";
+        // If team statistics should be pulled
+        static bool PULL_TEAM_STATISTICS = true;
+
+
+        // Stats to pull from 
+        static int[] PULL_TEAM_STATISTCS_SEASON = { 30 };
+        // Helper item
+        static int MIN_SEASON_THRESHOLD = PULL_TEAM_STATISTCS_SEASON.Min();
+
+        // Eager load mode attempts to pull all stats for a player by season immediately
+        // Most of the other projects depend on leaving this on.
+        static bool EAGER_LOAD_DATA_MODE = true;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            if (args.Length != 0)
+            {
+                throw new NotImplementedException("Command line arguments not yet supported");
+            }
+
+            var parser = new Parser(
+                targetSeason: "30",
+                seasonStatsToPull: new int[] { 30 }
+                );
+
+            parser.Parse();
+
         }
+
+
     }
 }
